1de1a82b83be37e19e7cd119ad851373e4e5f87c
MouBieCat <babyhongyilove910307@gmail.com>
2022-02-27 14:48:47 +0800

    使系統更加穩定，且添加訊息結果。

--- src/main/java/com/cat/server/listener/PlayerChatEvent.java
+++ src/main/java/com/cat/server/listener/PlayerChatEvent.java
@@ -32,6 +32,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.mineacademy.chatcontrol.api.CheckResult;
+import org.mineacademy.chatcontrol.api.EventCancelledException;
+import org.mineacademy.chatcontrol.listener.ListenerChecker;
 
 /**
  * 有關玩家聊天事件的監聽類
@@ -48,13 +51,52 @@ public final class PlayerChatEvent
     public void onChat(final @NotNull AsyncPlayerChatEvent event) {
         final Player player = event.getPlayer();
         final String message = event.getMessage();
+
+        if (this.playerChecker(player, message))
+            // 發送訊息
+            this.sendMessage(player, message);
+
+        // 無論如何取消事件
+        event.setCancelled(true);
+    }
+
+    /**
+     * 檢查玩家是否可以發送訊息
+     * @param player 玩家
+     * @param message 訊息
+     * @return 是否可以
+     */
+    private boolean playerChecker(final @NotNull Player player, final @NotNull String message) {
+        // 建立結果
+        final CheckResult result = new CheckResult(message, player);
+        try {
+            // 檢查結果
+            final @Nullable CheckResult resultBuffer = ListenerChecker.checkMessage(result);
+
+            // 如果原始訊息被修改
+            if (resultBuffer == null || resultBuffer.hasMessageChanged())
+                return false;
+
+        } catch (final EventCancelledException ignored) { return false; }
+        return true;
+    }
+
+    /**
+     * 發送訊息到指定頻道
+     * @param player 玩家
+     * @param message 訊息
+     */
+    private void sendMessage(final @NotNull Player player, final @NotNull String message) {
+        // 從訊息中提取快捷符號
         final String prefix = message.substring(0, 1);
 
+        // 獲取所有頻道
         final Channels manager = MouBieCat.getInstance().getChannelManager();
 
         // 獲取玩家前綴頻道快捷符號
         final @Nullable Channel channel = manager.get(prefix);
 
+        // 如果頻道不為空、檢查快捷符號符合
         if (channel != null && channel.checkPrefix(prefix))
             channel.sendMessage(player, message.substring(1));
 
@@ -64,9 +106,6 @@ public final class PlayerChatEvent
             if (defaultChannel instanceof DefaultChannel)
                 defaultChannel.sendMessage(player, message);
         }
-
-        // 無論如何取消事件
-        event.setCancelled(true);
     }
 
 }
